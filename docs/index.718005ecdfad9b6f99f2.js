33ba8f6e570c8bfc59dcb914ee6d9e51bb75748a
Olga Ostroverkh <ostrova22@mail.ru>
2024-06-06 13:47:25 +0300

    v1.7.1

---
+++ docs/index.718005ecdfad9b6f99f2.js
@@ -0,0 +1,45 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+// //импорт стилей
+
+$(document).ready(function () {
+  $('#S_screen1').click(function () {
+    $('.first_loop').addClass('None');
+    $('.video').get(0).play();
+    $('.C_videos').css('cursor', 'auto');
+    setTimeout(function () {
+      $('.video').addClass('None');
+      $('.C_cards').removeClass('None');
+      $('.final_loop').get(0).play();
+      $('.final_loop').css('loop', 'loop');
+    }, 3390);
+  });
+
+  // $('#contactEmail').click(function () {
+  //   var textToCopy = $(this).text()
+  //   copyToClipboard(textToCopy)
+  //   alert('Скопировано')
+  // })
+
+  // function copyToClipboard(text) {
+  //   var $temp = $('<textarea>')
+  //   $('body').append($temp)
+  //   $temp.val(text).select()
+  //   document.execCommand('copy')
+  //   $temp.remove()
+  // }
+
+  var button = document.querySelector('contactEmail'); //сама кнопка
+  $(function () {
+    $(document).on('click', 'button', function () {
+      navigator.clipboard.writeText('Blak.smoke.sol@gmail.com');
+      $('.A_copied').removeClass('None');
+      setTimeout(function () {
+        $('.A_copied').addClass('None');
+      }, 3390);
+    });
+  });
+});
+/******/ })()
+;
