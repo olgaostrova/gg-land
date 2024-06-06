ae6b71fbf0074229c8e86ed5745c88cebc5d12c7
Olga Ostroverkh <ostrova22@mail.ru>
2024-06-06 13:44:05 +0300

    v1.7

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,8 @@ $(document).ready(function () {
   $('#S_screen1').click(function () {
     $('.first_loop').addClass('None')
     $('.video').get(0).play()
+    $('.C_videos').css('cursor', 'auto')
+
     setTimeout(function () {
       $('.video').addClass('None')
       $('.C_cards').removeClass('None')
@@ -13,17 +15,28 @@ $(document).ready(function () {
     }, 3390)
   })
 
-  $('#contactEmail').click(function () {
-    var textToCopy = $(this).text()
-    copyToClipboard(textToCopy)
-    alert('Скопировано')
-  })
+  // $('#contactEmail').click(function () {
+  //   var textToCopy = $(this).text()
+  //   copyToClipboard(textToCopy)
+  //   alert('Скопировано')
+  // })
 
-  function copyToClipboard(text) {
-    var $temp = $('<textarea>')
-    $('body').append($temp)
-    $temp.val(text).select()
-    document.execCommand('copy')
-    $temp.remove()
-  }
+  // function copyToClipboard(text) {
+  //   var $temp = $('<textarea>')
+  //   $('body').append($temp)
+  //   $temp.val(text).select()
+  //   document.execCommand('copy')
+  //   $temp.remove()
+  // }
+
+  const button = document.querySelector('contactEmail') //сама кнопка
+  $(function () {
+    $(document).on('click', 'button', function () {
+      navigator.clipboard.writeText('Blak.smoke.sol@gmail.com')
+      $('.A_copied').removeClass('None')
+      setTimeout(function () {
+        $('.A_copied').addClass('None')
+      }, 3390)
+    })
+  })
 })
